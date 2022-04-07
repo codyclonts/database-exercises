2e7c3dc729ffc4362ab409fca7a1fe83e349bfa8
Cody Clonts <clontscody@gmail.com>
2022-04-07 13:57:43 -0500

    case_exercises updated

--- case_exercises.sql
+++ case_exercises.sql
@@ -29,8 +29,31 @@ ORDER BY last_name;
 select * from employees;
 
 SELECT 
-COUNT(CASE WHEN birth_date BETWEEN '1950' AND '1959' then birth_date ELSE NULL END) AS '1950s'
-END AS decade_count
+COUNT(CASE WHEN birth_date BETWEEN '1950-01-01' AND '1959-12-31' then birth_date ELSE NULL END) AS '1950s',
+COUNT(CASE WHEN birth_date BETWEEN '1960-01-01' AND '1969-12-31' then birth_date ELSE NULL END) AS '1960s',
+COUNT(CASE WHEN birth_date BETWEEN '1970-01-01' AND '1979-12-31' then birth_date ELSE NULL END) AS '1970s',
+COUNT(CASE WHEN birth_date BETWEEN '1980-01-01' AND '1989-12-31' then birth_date ELSE NULL END) AS '1980s',
+COUNT(CASE WHEN birth_date BETWEEN '1990-01-01' AND '1999-12-31' then birth_date ELSE NULL END) AS '1990s'
 from employees;
 
+## example
+SELECT CONCAT(SUBSTR(birth_date, 1, 3), '0') as decade,
+COUNT(*)
+FROM employees
+GROUP BY decade;
 
+#4
+
+SELECT 
+CASE WHEN d.dept_name IN ('Reasearch', 'Development') THEN 'Research & Development'
+WHEN d.dept_name IN ('Sales', 'Marketing') THEN 'Sales & Marketing'
+WHEN d.dept_name IN ('Production', 'Quality Management') THEN 'Product & Quality Management' 
+WHEN d.dept_name IN ('Finance' , 'Human Resouces' ) THEN 'Finance & HR'
+ELSE d.dept_name
+END AS dept_group, 
+AVG(s.salary) as avg_salary
+from departments as d
+join dept_emp as de using (dept_no)
+join salaries s using (emp_no)
+WHERE s.to_date > CURDATE() AND de.to_date > CURDATE()
+GROUP BY dept_group;
